1664c2a30f732b71827899b89eaf90c5369e3a7c
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-03-21 16:07:40 +0100

    Landingpage refactor

---
+++ src/components/Elements/HubspotSticker.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import StarIcon from '../../icons/star-fill.svg';
+import StickerIcon from '../../icons/hubspot-sticker.svg';
+import ArrowRight from '../../icons/arrow-right.svg';
+import { Link } from 'react-router-dom';
+
+const HubspotSticker = ( ) => {
+    return (
+      <div className='c-hubspot-sticker'>
+        <img className='c-hubspot-sticker__sticker-icon' src={StickerIcon} alt='HubSpot Review Sticker' />
+
+        <div className='c-hubspot-sticker__content'>
+            <div className='c-hubspot-sticker__rating u-flex u-flex-v-center'>
+                <img className='c-icon c-hubspot-sticker__star-icon' src={StarIcon} alt='Star Icon' />
+                <img className='c-icon c-hubspot-sticker__star-icon' src={StarIcon} alt='Star Icon' />
+                <img className='c-icon c-hubspot-sticker__star-icon' src={StarIcon} alt='Star Icon' />
+                <img className='c-icon c-hubspot-sticker__star-icon' src={StarIcon} alt='Star Icon' />
+                <img className='c-icon c-hubspot-sticker__star-icon' src={StarIcon} alt='Star Icon' />
+                <span className='c-hubspot-sticker__rating-title'>5/5</span>
+            </div>
+
+            <div className="c-hubspot-sticker__button-container">
+                <Link to='https://ecosystem.hubspot.com/marketplace/solutions/bright-digital' className='c-hubspot-sticker__button u-flex u-flex-v-center'>
+                    <span className="c-hubspot-sticker__button-title">
+                        Bekijk alle reviews
+                    </span>
+
+                    <img className='c-icon c-hubspot-sticker__star-icon' src={ArrowRight} alt='Arrow Right Icon' />
+                </Link>
+            </div>
+
+        </div>
+      </div>
+    );
+};
+  
+export default HubspotSticker;
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import brightDigitalLogo from '../images/logo-bright-zw.svg';
+
 import Button from '../components/elements/Button';
 import Form from '../components/form/Form';
 import Label from '../components/form/Label'; 
 import Input from '../components/form/Input';
+import HubspotSticker from '../components/elements/HubspotSticker';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <div className='v-landingpage u-flex'> 
-            <div className="v-landingpage-content__container-left u-bg-color--light-blue">
-                <div className="v-landingpage-content__container-left-inner">
-                    <div className='v-landingpage-content__header-container'>
-                        <img className="v-landingpage-content__bright-logo" src={brightDigitalLogo} alt="Bright Digital Logo" />
+        <div className='v-landingpage u-bg-color--light-blue'> 
+            <div className="o-container">
+                <div className="u-squeeze u-squeeze-xxxl">
+                    <div className='v-landingpage__header-container u-flex u-flex-sb u-flex-v-center'>
+                        <img className="v-landingpage__header__logo-left" src={brightDigitalLogo} alt="Bright Digital Logo" />
+
+                        <div className='v-landingpage__header__logo-right'>
+                            <HubspotSticker />
+                        </div>
                     </div>
+                </div>
 
-                    <p className='v-landingpage-content__p'>
-                        The #1 HubSpot integration to improve your sales
-                    </p>
+                <div className="v-landingpage__content u-flex u-flex-sb">
+                    <div className="v-landingpage__content-left">
+                        <span className='v-landingpage__content__payoff'>The #1 HubSpot integration to improve your sales</span>
 
-                    <h1 className='v-landingpage-content__h1-small'>
-                        Company.info + HubSpot
-                        <span className='v-landingpage-content__span'>Make your data Bright</span>
-                    </h1>
+                        <h1 className='v-landingpage__content__title'>Company.info + HubSpot</h1>
 
-                    <div className="v-landingpage-content__inner-content">
-                        <p>
-                            Discover the possibilities of connecting Company.Info with HubSpot and improve your sales process now!
+                        <p className='v-landingpage__content__text'>
+                            Discover the possiblities of connecting Company.Info with HubSpot and improve your sales process now!
                         </p>
 
                         <Form style='flex-column'>
@@ -36,28 +39,25 @@ const LandingPage = () => {
                             <Label text='Password' />
                             <Input type="password" name="name" />
 
-                            <div className="v-landingpage-content__form-bar u-flex u-flex-sb">
-                                <div className="v-landingpage-content__remember-me u-flex">
+                            <div className="v-landingpage__content__form-bar u-flex u-flex-sb">
+                                <div className="v-landingpage__content__form-bar__remember u-flex">
                                     <Input type='checkbox' />
-                                    <p className='v-landingpage-content__p-small'>Remember me</p>
+                                    <span className='v-landingpage__content__form-bar__remember-title'>Remember me</span>
                                 </div>
-                                <p className='v-landingpage-content__p-small--underline'>Forgot password?</p>
+                                <span className='v-landingpage__content__form-bar__password'>Forgot password?</span>
                             </div>
                         </Form>
 
-                        <Button title='Sign in' style='primary_submit' link='/install'/>
+                        <Button title='Sign in' style='primary_submit' link='/install' icon='ArrowRight' animation='move-right'/>
 
-                        <div className="v-landingpage-content__no-account u-flex u-flex-v-center">
-                            <p className='v-landingpage-content__no-account__title'>Don't have an account?&nbsp;</p>
-                            <Link to='/register' className='v-landingpage-content__no-account__title-secondary'>Sign up</Link>
+                        <div className="v-landingpage__content__no-account u-flex u-flex-v-center">
+                            <p className='v-landingpage__content__no-account__title'>Don't have an account?&nbsp;</p>
+                            <Link to='/register' className='v-landingpage__content__no-account__title-secondary'>Sign up</Link>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="v-landingpage-content__container-right u-bg-color--light-blue-alt">
-                <div className="v-landingpage-content__container-right-inner">
-                    <div className='v-landingpage-content__header-container v-landingpage-content__header-container--right'>
-                        <img className="v-landingpage-content__review" src={brightDigitalLogo} alt="Bright Digital Logo" />
+                    
+                    <div className="v-landingpage__content-right">
+                        <span className='v-landingpage-content__left-inner__payoff'>The #1 HubSpot integration to improve your sales</span>
                     </div>
                 </div>
             </div>
