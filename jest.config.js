d4cf73b4e3a8b406f047a2517170cf3fd1faa05d
Maurice <502241@student.saxion.nl>
2024-04-16 13:07:04 +0200

    Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  roots: ['<rootDir>'], 
-  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>'],
+  moduleFileExtensions: ['js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)$',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    'coverage',
+  ],
   verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  collectCoverage: process.env.npm_lifecycle_event === 'test:coverage',
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+  ],
 };
