27384013b886af6a1f8d2cbb9028fa095fa1234d
Maurice <502241@student.saxion.nl>
2024-04-24 08:55:46 +0200

    Properties delete and create, permanent version

--- src/views/Properties.js
+++ src/views/Properties.js
@@ -1,48 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import BreadCrumb from '../components/elements/BreadCrumb';
 import Button from '../components/elements/Button';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
-import { generatePropertyFields } from '../helpers/hubSpot/generatePropertyFields';
 import propertyService from '../services/propertyService';
+import groupService from '../services/groupService';
+import { generatePropertyFields } from '../helpers/hubSpot/generatePropertyFields';
 
 const CompanyDetail = () => {
   const authToken = useSelector((state) => state.auth.authToken);
   const [allProperties, setAllProperties] = useState([]);
   const [currentProperties, setCurrentProperties] = useState([]);
-  const [selectedProperties, setSelectedProperties] = useState([]); 
+  const [selectedProperties, setSelectedProperties] = useState(new Set());
 
   useEffect(() => {
-    const fetchPropertyFields = async () => {
+    const fetchData = async () => {
       try {
-        const properties = await generatePropertyFields();
-        setAllProperties(properties);
-  
-        const fetchedCurrentProperties = await propertyService.getProperties(
-          authToken,
-          'company',
-          'company_info_integration'
+        const allProps = await generatePropertyFields();
+        setAllProperties(allProps);
+
+        const fetchedCurrentProperties = await propertyService.getProperties(authToken, 'company', 'company_info_integration');
+        
+        const validCurrentProperties = fetchedCurrentProperties.filter((property) =>
+          allProps.some((ap) => ap.name === property.name)
         );
-  
-        const propertyNames = properties.map((property) => property.name);
-  
-        const validProperties = fetchedCurrentProperties.filter((property) =>
-          propertyNames.includes(property.name)
+
+        setCurrentProperties(validCurrentProperties);
+
+        const initialSelectedProperties = new Set(
+          validCurrentProperties.map((property) => property.name)
         );
-  
-        setCurrentProperties(validProperties);
-  
-        const selectedPropertyNames = validProperties.map((property) => property.name);
-        setSelectedProperties(selectedPropertyNames);
-  
+        setSelectedProperties(initialSelectedProperties);
       } catch (error) {
-        console.error('Error fetching property fields:', error);
+        console.error('Error fetching data:', error);
+        toast.error('Failed to fetch data. Please try again.');
       }
     };
-  
-    fetchPropertyFields();
+
+    fetchData();
   }, [authToken]);
+
+  const toggleProperty = (propertyName) => {
+    setSelectedProperties((prevSelected) => {
+      const updatedSelected = new Set(prevSelected);
+      if (updatedSelected.has(propertyName)) {
+        updatedSelected.delete(propertyName);
+      } else {
+        updatedSelected.add(propertyName);
+      }
+      return updatedSelected;
+    });
+  };
 
   const formatPropertyLabel = (str) => {
     return str
@@ -51,45 +60,40 @@ const CompanyDetail = () => {
       .join(' ');
   };
 
-  const toggleProperty = (propertyName) => {
-    setSelectedProperties((prevSelected) => {
-      if (prevSelected.includes(propertyName)) {
-        return prevSelected.filter((name) => name !== propertyName);
-      } else {
-        return [...prevSelected, propertyName];
-      }
-    });
-  };
-
   const handleSaveProperties = async () => {
     try {
-      const propertiesToDelete = currentProperties.filter(
-        (property) => !selectedProperties.includes(property.name)
-      );
-  
-      // const propertiesToCreate = selectedProperties.filter(
-      //   (propertyName) => !currentProperties.includes(propertyName)
-      // );
-  
-      if (propertiesToDelete.length > 0) {
-        console.log('Properties to delete:', propertiesToDelete);
+      let group = await groupService.getGroup(authToken, 'company', 'company_info_integration');
 
-        const deletedProperties = await propertyService.deleteProperties(authToken, 'company', propertiesToDelete[0].name);
-        if (deletedProperties) {
-          toast.success('Successfully deleted properties');
-          console.log(deletedProperties);
+      if (!group) {
+        group = await groupService.createGroup(authToken, 'company', 'company_info_integration');
+        if (!group) {
+          toast.error('No property group could be created, please contact an admin');
+          return;
         }
       }
-  
-      // if (propertiesToCreate.length > 0) {
-      //   console.log('Properties to create:', propertiesToCreate);
-      // }
-  
-      if (propertiesToDelete.length === 0) {
-      // if (propertiesToDelete.length === 0 && propertiesToCreate.length === 0) {
-        toast.success('No changes needed.');
-      } 
 
+      const propertiesToDelete = Array.from(currentProperties).filter(
+        (property) => !selectedProperties.has(property.name)
+      );
+
+      const propertiesToCreate = Array.from(allProperties).filter(
+        (property) => selectedProperties.has(property.name) && 
+          !currentProperties.some((cp) => cp.name === property.name)
+      );
+
+      for (const property of propertiesToDelete) {
+        await propertyService.deleteProperties(authToken, 'company', property.name);
+        toast.success('Successfully deleted properties')
+      }
+
+      if (propertiesToCreate.length > 0) {
+        await propertyService.createProperties(authToken, 'company', propertiesToCreate);
+        toast.success('Successfully created new properties');
+      }
+
+      if (propertiesToDelete.length === 0 && propertiesToCreate.length === 0) {
+        toast.success('All properties are up-to-date');
+      }
     } catch (error) {
       console.error('Error processing properties:', error);
       toast.error('Failed to process properties, please contact an admin');
@@ -107,7 +111,7 @@ const CompanyDetail = () => {
           <div className="v-properties__content-container__left">
             <h2 className="v-properties__title">Select properties</h2>
             <p className="v-properties__text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pharetra eros dui, et pellentesque est bibendum eget.
+              Select the properties that you would like to manage. 
             </p>
 
             <Button
@@ -122,27 +126,23 @@ const CompanyDetail = () => {
           <div className="v-properties__content-container__right u-flex">
             <div className="v-properties__titles-container u-flex">
               <div className="v-properties__hubspot-property-title-container">
-                <div className="v-properties__hubspot-properties-title">
-                  HubSpot property name
-                </div>
+                <div className="v-properties__hubspot-properties-title">HubSpot property name</div>
                 <div className="v-properties__line"></div>
               </div>
 
               <div className="v-properties__company-info-property-title-container">
-                <div className="v-properties__company-info-properties-title">
-                  Company.info property name
-                </div>
+                <div className="v-properties__company-info-properties-title">Company.info property name</div>
                 <div className="v-properties__line"></div>
               </div>
             </div>
 
             <div className="v-properties__properties-container">
               {allProperties.map((property, index) => (
-                <div key={index} className="v-properties__property-pair u-flex u-flex-v-center">
+                <div key={`${property.name}_${index}`} className="v-properties__property-pair u-flex u-flex-v-center">
                   <div className="v-properties__hubspot-property u-flex">
                     <input
                       type="checkbox"
-                      checked={selectedProperties.includes(property.name)}
+                      checked={selectedProperties.has(property.name)}
                       onChange={() => toggleProperty(property.name)}
                     />
                     <div className="v-properties__hubspot-property-label">
@@ -151,9 +151,7 @@ const CompanyDetail = () => {
                   </div>
 
                   <div className="v-properties__company-info-property u-flex u-flex-v-center">
-                    <div className="v-properties__company-info-label">
-                      {formatPropertyLabel(property.label)}
-                    </div>
+                    <div className="v-properties__company-info-label">{formatPropertyLabel(property.label)}</div>
                   </div>
                 </div>
               ))}
