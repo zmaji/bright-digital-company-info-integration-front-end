825beebd1f0b6282f9c6ccf88f3389538122a6d0
Maurice <502241@student.saxion.nl>
2024-05-01 09:15:08 +0200

    Changed HubSpot file folder from docs to scripts

--- src/data/HeaderScript.js
+++ src/data/HeaderScript.js
@@ -1,7 +1,7 @@
 const generateHeaderScript = (portalId) => {
     const headerScriptData = [
-        `<link rel="stylesheet" href="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/docs/Stylescript.css" />
-        <script type="text/javascript" src="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/docs/FormScript.js"></script>`
+        `<link rel="stylesheet" href="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/scripts/Stylescript.css" />
+        <script type="text/javascript" src="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/scripts/FormScript.js"></script>`
         ];
     
     return headerScriptData
