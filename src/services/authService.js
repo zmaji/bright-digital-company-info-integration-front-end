b849889a3d6feac68193e3e2b6f1a728829dca29
Maurice <502241@student.saxion.nl>
2024-05-07 08:13:38 +0200

    Changed back local env variables

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BACK_END_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const login = async (email, password) => {
   try {
