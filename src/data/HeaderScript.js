4c3737f279e7b6992e125be52fb942ea672686c0
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-05-23 13:57:28 +0200

    Stylescript typo

--- src/data/HeaderScript.js
+++ src/data/HeaderScript.js
@@ -1,6 +1,6 @@
 const generateHeaderScript = (portalId) => {
     const headerScriptData = [
-        `<link rel="stylesheet" href="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/scripts/Stylescript.css" />
+        `<link rel="stylesheet" href="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/scripts/StyleScript.css" />
         <script type="text/javascript" src="https://${portalId}.fs1.hubspotusercontent-eu1.net/hubfs/${portalId}/scripts/FormScript.js"></script>`
         ];
     
