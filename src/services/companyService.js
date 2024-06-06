cdfeeaf60d144c618b3181ad9030b376408312f5
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-06-06 13:55:18 +0200

    WIP: Format last sync date to include time

--- src/services/companyService.js
+++ src/services/companyService.js
@@ -4,9 +4,12 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const formatDate = (date) => {
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); 
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 };
 
 const setAuthorizationHeader = async (authToken) => {
