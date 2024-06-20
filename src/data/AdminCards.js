3bd872adbfab606a86326f1f2c52ee945229c35b
Maurice ten Teije <mauricetenteije@MacBook-Pro-van-Maurice.local>
2024-06-20 13:07:06 +0200

    Admin panel home cards styling

--- src/data/AdminCards.js
+++ src/data/AdminCards.js
@@ -2,7 +2,31 @@ const adminCardsData = [
   {
     icon: 'Users',
     title: 'View users',
-    text: 'View all users.',
+    text: 'Explore detailed information about all users in the system and manage their profiles efficiently.',
+    button: {
+      title: 'Start now',
+      style: 'tertiary',
+      link: '/users',
+      icon: 'ArrowRight',
+      animation: 'move-right'
+    }
+  },
+  {
+    icon: 'Users',
+    title: 'Create user',
+    text: 'Add new users to your system and manage their account specifications.',
+    button: {
+      title: 'Start now',
+      style: 'tertiary',
+      link: '/users/create',
+      icon: 'ArrowRight',
+      animation: 'move-right'
+    }
+  },
+  {
+    icon: 'Users',
+    title: 'Delete user',
+    text: 'Remove users from your system and keep your user base up-to-date.',
     button: {
       title: 'Start now',
       style: 'tertiary',
